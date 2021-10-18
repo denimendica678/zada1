4311d871f13892879f55b2786c19cb94f4d849e3
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-10-18 09:25:25 +0200

    Add files via upload

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport nogomet = new Sport(11, "Nogomet", true);
+            Sport vaterpolo = new Sport(7, "Vaterpolo", true);
+            Sport curling = new Sport(4, "Curling", true);
+
+            
+        }
+
+    }
+
+}
---
+++ zad1/zad1/Sport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    public class Sport
+    {
+
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public Sport(string naziv)
+        {
+
+            Naziv = naziv;
+
+        }
+
+        public Sport(string naziv, bool igraLiSeLoptom)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+        }
+
+        public Sport(string naziv, bool igraLiSeLoptom, byte brojIgraca)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+            BrojIgraca = brojIgraca;
+        }
+
+        public string Naziv1 { get => Naziv; set => Naziv = value; }
+        public bool IgraLiSeLoptom1 { get => IgraLiSeLoptom; set => IgraLiSeLoptom = value; }
+        public byte BrojIgraca1 { get => BrojIgraca; set => BrojIgraca = value; }
+    }
+}
